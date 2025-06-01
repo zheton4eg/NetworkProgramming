ad9e4495119f0909bc350a48af88bc7912593d7a
zheka <zheka@HOME-PC>
2025-06-01 13:50:14 +0300

    IpCalc_STARTED

---
+++ NetWorkProgramming/IPcalc/main.cpp
@@ -0,0 +1,24 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+
+	return FALSE;
+}
--- NetWorkProgramming/IPcalc/resource.h
+++ NetWorkProgramming/IPcalc/resource.h
@@ -3,11 +3,13 @@
 // Used by Resource.rc
 //
 #define IDC_STATIC_IP_ADDRESS           101
+#define IDD_DIALOG1                     101
 #define IDC_STATIC_IP_MASK              1001
 #define IDC_STATIC_IP_PREFIX            1002
 #define IDC_IPADDRESS                   1003
-#define IDC_IPADDRESS2                  1004
 #define IDC_IPMASK                      1004
+#define IDC_EDIT_PREFIX                 1005
+#define IDC_SPIN1                       1006
 
 // Next default values for new objects
 // 
@@ -15,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
